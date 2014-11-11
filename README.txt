5c8e68b8a539642fac0f00a71258f3f24e7c3dc5
Angela Liu <angelamuliu@gmail.com>
2014-11-11 00:50:10 -0500

    Added speech to text capabilities to Stock Activity. Right now, tap to start listening. Then it displays what it thinks you said. And you can tap the list items

--- README.txt
+++ README.txt
@@ -1,6 +1,8 @@
 
 
 References
+A HUGE thank you to these great online sources! Starred are ones that I especially
+referenced from.
 ----------------
 Transitions between activities
 	http://stackoverflow.com/questions/3389501/activity-transition-in-android
@@ -10,3 +12,17 @@ Transition clicking back
 	http://stackoverflow.com/questions/12047770/android-how-to-animate-an-activity-transition-when-the-default-back-button-is
 Transition clicking 'up' button
 	http://stackoverflow.com/questions/17854528/actionbar-up-button-transition-effect
+	
+Centering content in layouts
+	http://stackoverflow.com/questions/1957831/centre-a-button-in-a-linear-layout
+	
+Speech to Text
+	http://www.androidhive.info/2014/07/android-speech-to-text-tutorial/
+	* http://www.jameselsey.co.uk/blogs/techblog/android-how-to-implement-voice-recognition-a-nice-easy-tutorial/
+	
+ArrayAdapters with Listviews
+	https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
+
+Item Click Listeners in Listviews
+	http://stackoverflow.com/questions/12328804/listview-setonitemclicklistener-not-working-for-custom-listview-but-working-in
+	* http://www.ezzylearning.com/tutorial/handling-android-listview-onitemclick-event
--- src/edu/cmu/amliu/stockpile/Stock.java
+++ src/edu/cmu/amliu/stockpile/Stock.java
@@ -1,18 +1,45 @@
 package edu.cmu.amliu.stockpile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Stock extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Intent intent = getIntent();
 		setContentView(R.layout.activity_stock);
+		
+		// Setup interactive elements, speak button and list that displays results
+		Button speakButton = (Button) findViewById(R.id.speakbutton);
+        wordsList = (ListView) findViewById(R.id.list);
+        
+     // Disable button if no recognition service is present
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        if (activities.size() == 0) {
+        	speakButton.setEnabled(false);
+            speakButton.setText("Recognizer not present");
+        }
 	}
 
 	@Override
@@ -46,5 +73,61 @@ public class Stock extends Activity {
 	    super.onBackPressed();
 	    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
 	}
+	
+	// ------------------------------------------
+	// SPEECH TO TEXT MANAGEMENT
+	// ------------------------------------------
+	
+    private static final int REQUEST_CODE = 1234;
+    private ListView wordsList;
+	
+	// Called on speak button click
+	public void speakbutton_clicked(View v) {
+		startVoiceRecognitionActivity();
+	}
+   
+	// Fire intent to start voice recognition
+	private void startVoiceRecognitionActivity() {
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition Demo...");
+		startActivityForResult(intent, REQUEST_CODE);
+	}
+	
+	// Handle voice recognition results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK)
+        {
+            // Populate the wordsList with the String values the recognition engine thought it heard
+        	// i.e. "Milk" -> matches = [milk, Milk, Mielke]
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            
+            Log.d("Hearing...", matches.toString());
+            
+            // ArrayAdapters convert an array into a set of views that can be loaded into a listview
+            ArrayAdapter<String> wordAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, matches);
+            wordsList.setAdapter(wordAdapter);
+            // Update wordsList with the new set of possible words from listener
+            
+            // The list has been generated. Now we can set a listener per list item (in this case,
+            // android's best speech recognition guesses) and do something with them
+            wordsList.setOnItemClickListener(new OnItemClickListener() {
+                
+            	public void onItemClick(AdapterView<?> parent, View view, int position,
+                        long id) {
+                    
+                    String item = ((TextView)view).getText().toString();
+                    // For now, just show a toast message of the tapped item
+                    Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+                    
+                }
+            });
+            
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    
 	
 }
