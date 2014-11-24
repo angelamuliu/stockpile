f0afdba524ae70cee03e54fe6c8f9f70b593793e
Angela Liu <angelamuliu@gmail.com>
2014-11-24 17:17:54 -0500

    Added validation for food fields in textcraft activity

--- src/edu/cmu/amliu/stockpile/TextcraftActivity.java
+++ src/edu/cmu/amliu/stockpile/TextcraftActivity.java
@@ -1,13 +1,17 @@
 package edu.cmu.amliu.stockpile;
 
-import edu.cmu.amliu.stockpile.db.Food;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /*
  * Similar to the Speechcraft Activity, but instead simply displays a text input box
@@ -18,32 +22,80 @@ import android.view.View;
 
 public class TextcraftActivity extends Activity {
 	
-	private Food newFood;
+	private String location = "";
+	private String foodName = "";
+	private String[] foodValues;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		Intent intent = getIntent();
 		Bundle bundle = intent.getExtras();
 
 		String[] testarray = bundle.getStringArray("test bundle");
-		
 		Log.d("Testing sending array between activities", testarray[0]);
 		
-		newFood = new Food();
 		setContentView(R.layout.activity_textcraft);
 	}
 
 	// When the user is all done, press the addfood button which calls this
-	// which packages info to send back to stock activity (and finishes this activity)
+	// which packages info to send back to stock activity
 	public void addFood(View view) {
-		Intent resultIntent = new Intent();
-		resultIntent.putExtra("Add food", "From Add Food");
-		setResult(Activity.RESULT_OK, resultIntent);
-		finish();
-	    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+		Context context = getApplicationContext();
+		if (foodName.isEmpty()) {
+			CharSequence text = "Type in food";
+			int duration = Toast.LENGTH_SHORT;
+
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+		} else if (location.isEmpty()) {
+			CharSequence text = "Select a location";
+			int duration = Toast.LENGTH_SHORT;
+
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+		} else {
+			Log.d("Food name", foodName);
+			Log.d("location", location);
+		}
+//		Intent resultIntent = new Intent();
+//		resultIntent.putExtra("Add food", "From Add Food");
+//		setResult(Activity.RESULT_OK, resultIntent);
+//		finish();
+//	    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
 	}
+	
+	// Clicking OK next to the text input sets the name
+	public void setFoodName(View view) {
+		EditText foodInput = (EditText) findViewById(R.id.foodInput);
+		foodName = foodInput.getText().toString();
+		
+		Context context = getApplicationContext();
+		// Remove focus from keyboard
+		InputMethodManager imm = (InputMethodManager) context.getSystemService(context.INPUT_METHOD_SERVICE);
+	    imm.hideSoftInputFromWindow(foodInput.getWindowToken(), 0);
+	}
+	
+	// Setting location
+	public void setOutside(View view) {
+		location = "Outside";
+		Log.d("SET", location);
+	}
+	
+	public void setFridge(View view) {
+		location = "Fridge";
+		Log.d("SET", location);
+	}
+
+	public void setFreezer(View view) {
+		location = "Freezer";
+		Log.d("SET", location);
+	}
+
+	
+	
+	// ------------------------------------------
+	// Default stuff, remove if necessary
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
